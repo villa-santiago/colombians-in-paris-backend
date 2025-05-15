4fcf9d81ce3def9e0c053c9478a73478792f70da
Santiago Villa <code@santiagovilla.it>
2025-05-15 20:59:30 +0200

    updated app.js

--- app.js
+++ app.js
@@ -5,15 +5,18 @@ const morgan = require('morgan');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(morgan('dev'));
+
 server.use((req, res, next) => {
-  
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
+
 server.use(router);
 
 server.listen(PORT, () => {
