38197635debb160f93a9d45eb68056cfdb4c274d
Santiago Villa <code@santiagovilla.it>
2025-05-15 19:58:11 +0200

    actualizacion version json server

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT;
 server.use(middlewares);
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  // Middleware to disable CORS
+  
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
